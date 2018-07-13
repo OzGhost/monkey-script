88c40719d4f59a54e3dffe16ba745fcd2145b100
Oz Ghost <xad.love@gmail.com>
2018-07-13 23:07:22 +0700

    test fetch response v2

--- portal_custom_shortcut.js
+++ portal_custom_shortcut.js
@@ -1,7 +1,1 @@
 
-fetch('https://raw.githubusercontent.com/OzGhost/monkey-script/master/di.js')
-.then(function(rs){
-  console.log("cout << got rs: ", rs, arguments)
-}, function(err){
-  console.log("cout << got err: ", err, arguments)
-})
