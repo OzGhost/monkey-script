f61279323865535fe58eb159d30dc0bbe911e862
Oz Ghost <xad.love@gmail.com>
2018-07-13 23:19:08 +0700

    change script loader

--- script_loader.js
+++ script_loader.js
@@ -1,18 +1,8 @@
 // ==UserScript==
 // @name     no-name
 // @version  1
-// @grant    GM.getResourceUrl
-// @resource portal_custom_shortcut https://raw.githubusercontent.com/OzGhost/monkey-script/master/portal_custom_shortcut.js
+// @grant    none
+// @require https://raw.githubusercontent.com/OzGhost/monkey-script/master/di.js
 // ==/UserScript==
 
-GM.getResourceUrl("portal_custom_shortcut")
-.then(function(rs){
-  var tag = document.createElement("SCRIPT")
-  tag.src = rs
-  tag.type = "text/javascript"
-  document.body.appendChild(tag)
-  console.log("cout << script [portal_custom_shortcut] loaded.")
-}, function(err){
-  console.log("cout << fail to load script! [portal_custom_shortcut]")
-})
 
