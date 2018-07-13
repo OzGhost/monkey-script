4db645fbfccdb62c58e06709c931d4fd3f6ba119
Oz Ghost <xad.love@gmail.com>
2018-07-13 23:03:37 +0700

    test fetch response

---
+++ di.js
@@ -0,0 +1,43 @@
+
+window.DI = function(){
+  var _this = this
+
+  this.dsa = []
+  
+  this.load = function(dependencies) {
+    _this.dsa = _this.preprocess(dependencies)
+
+    var len = _this.dsa.length
+    for (var i = 0; i < len; i += 2) {
+      _this.loadSingleDependency(_this.dsa[i], function() {
+        _this.dsa[i+1] = 1
+      })
+    }
+  }
+
+  this.preprocess = function(dependencies) {
+    if (typeof( dependencies ) === 'undefined')
+      return []
+    var dsa = []
+    for (var dependency in dependencies) {
+      dsa.push(dependency)
+      dsa.push(0)
+    }
+    return dsa
+  }
+
+  this.loadSingleDependency = function(dependency, callback) {
+    fetch(_this.buildDependencyUrl(dependency))
+      .then()
+  }
+
+  this.buildDependencyUrl = function(dependency) {
+    return 'https://raw.githubusercontent.com/OzGhost/monkey-script/master/'
+          + dependency
+          + '.js'
+  }
+
+  return {
+    load
+  }
+}
--- portal_custom_shortcut.js
+++ portal_custom_shortcut.js
@@ -1,2 +1,7 @@
 
-window.alert("cout << You got it!");
+fetch('https://raw.githubusercontent.com/OzGhost/monkey-script/master/di.js')
+.then(function(rs){
+  console.log("cout << got rs: ", rs, arguments)
+}, function(err){
+  console.log("cout << got err: ", err, arguments)
+})
